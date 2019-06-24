9d7da532b75acdbb56b63ceee3f6dc67629b034b
Aaron Brezel <35546183+aaronbrezel@users.noreply.github.com>
2019-06-24 17:49:28 -0400

    add lat/long values to projects

--- clearn_build_data.R
+++ clearn_build_data.R
@@ -1,7 +1,10 @@
 library("dplyr")
 library("jsonlite")
 
-all_grants <- read.csv("C:/Users/aaron/Downloads/Florida TIGER_BUILD Grants.csv") #reads in csv
+#all_grants <- read.csv("C:/Users/aaron/Downloads/Florida TIGER_BUILD Grants.csv") #reads in csv
+
+all_grants <- read.csv("C:/Users/abrezel/Documents/GitHub/Approved-TIGER-BUILD-Projects-Florida/Florida TIGER_BUILD Grants.csv")
+
 
 all_grants <- as.data.frame(all_grants) #converts list to data frame
 
@@ -10,6 +13,12 @@ all_grants[,9] <- as.character(all_grants[,9]) #sets Grant Amount Column to char
 all_grants[,2] <- as.integer(all_grants[,2])
 
 approved_grants <- all_grants %>% filter(GrantAmount != "#N/A")
+
+lat <- c(28.537971, 25.908644, 27.636500, 30.398273, 26.637771, 26.12367, 27.947055, 26.330512, 26.350433, 25.764509, 25.888879, 26.1849, 26.410578, 25.584623, 30.328843)								
+long <- c(-81.391789, -80.162601, -82.554475, -81.566218, -81.031757, -80.143261, -82.459609, -81.805777, -80.08801, -80.368607, -80.513306, -80.2296, -81.416571, -80.373677, -81.658669)
+
+approved_grants$latitude <- lat
+approved_grants$longitude <- long
 
 approved_grants$GrantAmount <- as.numeric(gsub("[\\$,]", "", approved_grants$GrantAmount))
 approved_grants$Funding.Request <- as.numeric(gsub("[\\$,]", "", approved_grants$Funding.Request))
@@ -23,9 +32,6 @@ approved_grants$Project.Type <- factor(approved_grants$Project.Type, levels = c(
 
 approved_grants <- arrange(approved_grants, approved_grants$Year, approved_grants$Project.Type) #sorts by year and project type
 
-approved_grants[0,2]
-
-approved_grants[2,"GrantAmount"]
 
 old_year <- 2009
 year_total <- 0
@@ -44,4 +50,7 @@ for (row in 1:nrow(approved_grants)){ ##sets bar total to make assembling the d3
 }
 
 
-write_json(approved_grants, "C:/Users/aaron/Downloads/approvedfloridagrants.json")
+#write_json(approved_grants, "C:/Users/aaron/Downloads/approvedfloridagrants.json")
+
+
+write_json(approved_grants, "C:/Users/abrezel/Documents/GitHub/Approved-TIGER-BUILD-Projects-Florida/approvedfloridagrants.json")
